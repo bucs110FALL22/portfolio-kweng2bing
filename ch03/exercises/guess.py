bf5cf1d7d0d5848dbc43807ae283efe62fabb9a1
Kalvin <111814372+kweng2bing@users.noreply.github.com>
2022-09-22 18:48:40 +0000

    Lab 3 Complete

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -17,20 +17,15 @@ while (tries < 3 and tries >= 0):
 if tries == 3:
   print("Better luck next time")
 
-
-# chance = 3
+# num = random.randrange(1,11)
+# num_guesses = 0
 # for i in range(chance):
-#   if tries == 3: 
-#     print("Sorry to many tries")
-#     tries = 3
-#   else: 
-#     num = int(input("Guess a number form 1 to 10"))
-#     if num == rnum:
-#       print("Your correct")
-#       chance = tries
-#     elif num > rnum:
-#       print( str(num), "is too high. Guess a lower number")
-#       tries = tries + 1
-#     elif num < rnum:
-#       print(str(num), "is too low. Guess a higher number")
-#       tries= tries + 1
+#   guess_num = int(input("Guess a number form 1 to 10"))
+#   num_guesses += 1
+#   if guess_num > num:
+#     print("your guess is to high")
+#   elif guess_num < num:
+#     print("your guess is too low")
+#   else:
+#     print("Correct")
+#     break
--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,57 +1,64 @@
-import turtle #1. import modules
+#1. import modules
+import turtle 
 import random
 import pygame
 import math
-
-
 #Part A
-# window = turtle.Screen() # 2.  Create a screen
-# window.screensize(2000,2000)
-# window.bgcolor('lightblue')
+ # 2.  Create a screen
+window = turtle.Screen()
+window.screensize()
+window.bgcolor('lightblue')
+ # 3.  Create two turtles
+michelangelo = turtle.Turtle()
+leonardo = turtle.Turtle()
+michelangelo.color('orange')
+leonardo.color('blue')
+michelangelo.shape('turtle')
+leonardo.shape('turtle')
+# 4. Pick up the pen so we don’t get lines
+michelangelo.up()
+leonardo.up()
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
-# michelangelo = turtle.Turtle() # 3.  Create two turtles
-# leonardo = turtle.Turtle()
-# michelangelo.color('orange')
-# leonardo.color('blue')
-# michelangelo.shape('turtle')
-# leonardo.shape('turtle')
+## 5. Your PART A code goes here
+random1 = random.randrange(0,101)
+random2 = random.randrange(0,101)
+michelangelo.forward(random1)
+leonardo.forward(random2)
+print(random1)
+print(random2)
 
-# michelangelo.up() # 4. Pick up the pen so we don’t get lines
-# leonardo.up()
-# michelangelo.goto(-100,20)
-# leonardo.goto(-100,-20)
-
-# ## 5. Your PART A code goes here
-# random1 = random.randrange(0,101)
-# random2 = random.randrange(0,101)
-# leonardo.forward(random1)
-# leonardo.forward(random2)
-
-# for i in range(10):
-#   random1 = random.randrange(0,10)
-#   print("Turtle 1 moved" , str(random1))
-#   leonardo.forward(random1)
-#   random2 = random.randrange(0,10)
-#   print("Turtle 2 moved" , str(random2))
-#   leonardo.forward(random2)
-# michelangelo.goto(-100,20)
-# leonardo.goto(-100,-20)
-# window.exitonclick()
+for i in range(10):
+  random1 = random.randrange(0,10)
+  print("Turtle 1 moved" , str(random1))
+  michelangelo.forward(random1)
+  random2 = random.randrange(0,10)
+  print("Turtle 2 moved" , str(random2))
+  leonardo.forward(random2)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+window.exitonclick()
 # PART B - complete part B here
 pygame.init()
-screen =  pygame.display.set_mode()
-screen.fill([255, 0, 0])
-pygame.display.flip()
-
-pygame.time.wait(500)
-
-screen.fill([0, 255, 0])
-pygame.display.flip()
-
-# pygame.time.wait(500)
-
-# screen.fill([0, 0, 255])
-# pygame.display.flip()
-
-
-
+window = pygame.display.set_mode()
+coords = []
+num_sides = [3,4,6,9,360]
+side_length = 80
+offset = 100
+color = ""'cyan'""
+for i in num_sides:
+  for shape in range(int(i)):
+    theta = (2.0 * math.pi * (shape)) / i
+    x = side_length * math.cos(theta) + offset
+    y = side_length * math.sin(theta) + offset
+    coords.append([x , y])
+  # display shape
+  pygame.draw.polygon(window, color, coords)
+  pygame.display.flip()
+  pygame.time.wait(1000)
+  coords = []
+  #reset screen
+  window.fill('black')
+  pygame.display.flip()
+  pygame.time.wait(1500)
