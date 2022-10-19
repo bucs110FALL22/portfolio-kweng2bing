ed7b620582261af7aa6482f3c51413feadc75ace
kweng2 <kweng2@binghamton.edu>
2022-10-19 17:42:07 -0400

    ch05

--- ch05/exercises/scope.py
+++ ch05/exercises/scope.py
@@ -1,23 +1,28 @@
-number1= int(input("Number 1?")) 
-number2= int(input("Number 2?"))
-
 def multiply(num1, num2):
   number= 0
   for i in range(num2):
     number+= num1
-  print(f"Multiply result: {number}")
-
-multiply(number1, number2)
+  return number
 
 def exponent(num1, num2):
   number = 1
   for i in range(num2):
     number*= num1
-  print(f"Exponenet Result:{number}")
-
-exponent(number1, number2)
+  return number 
 
 def square(num1):
-  number = num1* num1
-  print(f"Square Result:{number}")
-square(number1)
+  return multiply(num1, num1)
+
+def main():
+  numberone = int(input("Number 1?"))
+  numbertwo = int(input("Number 2?"))
+  result = multiply(numberone, numbertwo)
+  print(result)
+  print("===")
+  result = exponent(numberone, numbertwo)
+  print(result)
+  print("===")
+  result = square(numberone)
+  print(result)
+
+main()
