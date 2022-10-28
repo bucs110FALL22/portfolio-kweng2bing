7764aeca006cf4cb82d6d4b4985c9fbdf5d3ea12
Kalvin <111814372+kweng2bing@users.noreply.github.com>
2022-10-28 23:34:26 +0000

    Updated midterm

--- ch06/midterm/README.md
+++ ch06/midterm/README.md
@@ -1,9 +1,17 @@
 # CS110 Midterm Exam
 
 ## SHORT DESCRIPTION *(1 or 2 sentences describing your program)*
+A simple tictactoe game made with import turtle. I know there are better ways to make it, but the assignment requres turtle so my only choice was to make it with turtle.
 
 ## KNOWN BUGS AND INCOMPLETE PARTS *(list any known bugs or non-working parts)*
+This may or may not be fixed but players can choose to fill in a box more than once. In other one player 1 and player 2 can both choose a1.
 
 ## REFERENCES *(any resources you use outside of class materials)*
 
+https://bobbyhadz.com/blog/python-check-if-multiple-values-in-list#:~:text=Use%20the%20all()%20function,the%20list%20and%20False%20otherwise.
+https://docs.python.org/3/library/turtle.html#turtle.hideturtle
+https://www.geeksforgeeks.org/draw-color-filled-shapes-in-turtle-python/
+https://stackoverflow.com/questions/3931541/how-to-check-if-all-of-the-following-items-are-in-a-list
+
 ## MISCELLANEOUS COMMENTS *(anything else you would like the grader to know)*
+Interactive so please don't assume this program is only drawing two 4 lines. This assignemnt kind of deviates from the purpose of the assingment which was to draw, but it's nice to create a tic tac toe game. Also, unique in my opinion
--- ch06/midterm/midterm.py
+++ ch06/midterm/midterm.py
@@ -27,54 +27,105 @@ def tictactoeboard(ab):
   draw.ht()
 def shape1(pos):
   shape1 = turtle.Turtle()
+  shape1.shape('turtle')
   shape1.penup()
   shape1.goto(pos)
   shape1.pendown()
+  shape1.fillcolor('purple')
+  shape1.begin_fill()
   shape1.circle(40)
-
+  shape1.end_fill()
+  shape1.ht()
 def shape2(pos):
   shape2 = turtle.Turtle()
   shape2.penup()
   shape2.goto(pos)
+  shape2.fillcolor('lightblue')
+  shape2.begin_fill()
   shape2.pendown()
   shape2.right(90)
   for i in range(5):
     shape2.right(144)
     shape2.forward(80)
-
+  shape2.end_fill()
+  shape2.ht()
 def topos(input): 
-  dict = {
-  "a1": (-100,60),
-  "a2": (0, 60),
-  # a3: (-100, 60),
-  # b1: (-100, -40),
-  # b2: (0,-40),
-  # b3: (100, -40),
-  # c1: (-100, -140),
-  # c2: (0, -140),
-  # c3: (100, -140)
-}
+  dict = [
+  ["a1", (-100,60)],
+  ["a2", (0, 60)],
+  ["a3", (100, 60)],
+  ["b1", (-100, -40)],
+  ["b2", (0,-40)],
+  ["b3", (100, -40)],
+  ["c1", (-100, -140)],
+  ["c2", (0, -140)],
+  ["c3", (100, -140)]
+]
   for posarg in dict:
     if posarg[0] == input:
       return posarg[1]
-      print(posarg[1])
-      print(type(posarg[1]))
-      print(input)
-  
-
+def game():
+  gameOver = False
+  count = 0
+  player1choice =[]
+  player2choice =[]
+  while gameOver == False:
+    print("Type a1,a2,a3,b1,b2,b3,c1,c2, or c3")
+    for i in range(2):
+      if count == 8 and i ==0:
+        print("Player 1 Turn. Type a1,a2,a3,b1,b2,b3,c1,c2, or c3")
+        choice = input("Which box?")
+        pos = topos(choice)
+        shape1(pos)
+        player1choice.append(choice)
+        gameOver = True 
+        break
+      if i == 0:
+        print("Player 1 Turn.")
+        choice = input("Which box?")
+        pos = topos(choice)
+        shape1(pos)
+        player1choice.append(choice)
+        result = win(player1choice)
+        if result == True:
+          gameOver = True
+          print("Player 1 wins")
+          break
+        count += 1
+      if i == 1:
+        print("Player 2 Turn.")
+        choice = input("Which box?")
+        pos = topos(choice)
+        shape2(pos)
+        player2choice.append(choice)
+        result= win(player2choice)
+        if result == True:
+          gameOver = True
+          print("Player 2 wins")
+          break
+        count += 1
+def win(playerset):
+  winSet = [
+    ['a1','a2','a3'],
+    ['b1','b2','b3'],
+    ['c1','c2','c3'],
+    ['a1','b1','c1'],
+    ['a2','b2','c2'],
+    ['a3','b3','c3'],
+    ['a1','b2','c3'],
+    ['a3','b2','c1'],
+  ]
+  for i in winSet:
+    if all(value in playerset for value in i):
+      return True
+    
   
 def main():
   display = turtle.Screen()
   displaysize=400
   display.screensize(canvwidth=displaysize, canvheight=displaysize)
   tictactoeboard(displaysize)
-  shape1((0,-40))
-  shape2((100,-40))
-  input1 = input("Test?")
-  pos = topos(input1)
-  print(pos)
-  print(type(pos))
-  shape1(pos)
+  game()
   display.exitonclick()
   
 main()
