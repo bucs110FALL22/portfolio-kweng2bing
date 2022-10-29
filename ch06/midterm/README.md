222d1ede89fd7c50689f11dd4e3a594533466a98
Kalvin <111814372+kweng2bing@users.noreply.github.com>
2022-10-29 00:41:36 +0000

    Midterm Complete

--- ch06/midterm/README.md
+++ ch06/midterm/README.md
@@ -4,7 +4,8 @@
 A simple tictactoe game made with import turtle. I know there are better ways to make it, but the assignment requres turtle so my only choice was to make it with turtle.
 
 ## KNOWN BUGS AND INCOMPLETE PARTS *(list any known bugs or non-working parts)*
-This may or may not be fixed but players can choose to fill in a box more than once. In other one player 1 and player 2 can both choose a1.
+Players can choose to fill in a box more than once. In other words, player 1 and player 2 can both choose a1.
+Also, if you type something incorrectly it will kick you out of the program.
 
 ## REFERENCES *(any resources you use outside of class materials)*
 
@@ -14,4 +15,5 @@ https://www.geeksforgeeks.org/draw-color-filled-shapes-in-turtle-python/
 https://stackoverflow.com/questions/3931541/how-to-check-if-all-of-the-following-items-are-in-a-list
 
 ## MISCELLANEOUS COMMENTS *(anything else you would like the grader to know)*
-Interactive so please don't assume this program is only drawing two 4 lines. This assignemnt kind of deviates from the purpose of the assingment which was to draw, but it's nice to create a tic tac toe game. Also, unique in my opinion
+Interactive so please don't assume this program is only drawing  4 lines. This project kind of deviates from the purpose of the assingment which was to draw something, but it's nice to create a tic tac toe game. Also, unique in my opinion. 
+I was thinking of also making an AI bot to go along with the program, but I do not have enough time. I will probably do it in my free time.
--- ch06/midterm/midterm.py
+++ ch06/midterm/midterm.py
@@ -1,22 +1,20 @@
 import turtle
 
-def tictactoeboard(ab):
+def tictactoeboard(displaysize):
+# A function that sets up the board
   draw = turtle.Turtle()
   draw.color('black')
   draw.penup()
-  fromedge = ab/8
-  print(f"distancefromline{fromedge}")
-  a = (ab- (fromedge * 2)) /3
-  print(a)
-  coordsleft=[(-(fromedge + a), fromedge), (-(fromedge + a), -fromedge)]
+  fromedge = displaysize/8
+  distance = (displaysize- (fromedge * 2)) /3
+  coordsleft=[(-(fromedge + distance), fromedge), (-(fromedge + distance), -fromedge)]
   print(coordsleft)
   for i in range(len(coordsleft)): 
-    print(i)
     draw.goto(coordsleft[i])
     draw.pendown()
     draw.forward(300)
     draw.penup()
-  coordstop = [(-fromedge,(fromedge + a)), (fromedge, (fromedge+ a))]
+  coordstop = [(-fromedge,(fromedge + distance)), (fromedge, (fromedge + distance))]
   for i in range(len(coordstop)): 
     draw.goto(coordstop[i])
     draw.pendown()
@@ -25,7 +23,9 @@ def tictactoeboard(ab):
     draw.left(90)
     draw.penup()
   draw.ht()
+
 def shape1(pos):
+# A function for drawing Player 1's shape == circle
   shape1 = turtle.Turtle()
   shape1.shape('turtle')
   shape1.penup()
@@ -36,7 +36,10 @@ def shape1(pos):
   shape1.circle(40)
   shape1.end_fill()
   shape1.ht()
+
+# Player 2 shape drawing
 def shape2(pos):
+  # A function that draws Player 2's shape == star
   shape2 = turtle.Turtle()
   shape2.penup()
   shape2.goto(pos)
@@ -50,6 +53,7 @@ def shape2(pos):
   shape2.end_fill()
   shape2.ht()
 def topos(input): 
+  # A function used to translate a1...c3 to coordinates.
   dict = [
   ["a1", (-100,60)],
   ["a2", (0, 60)],
@@ -65,6 +69,7 @@ def topos(input):
     if posarg[0] == input:
       return posarg[1]
 def game():
+# A function for the overall game
   gameOver = False
   count = 0
   player1choice =[]
@@ -73,7 +78,7 @@ def game():
     print("Type a1,a2,a3,b1,b2,b3,c1,c2, or c3")
     for i in range(2):
       if count == 8 and i ==0:
-        print("Player 1 Turn. Type a1,a2,a3,b1,b2,b3,c1,c2, or c3")
+        print("Player 1 Turn.")
         choice = input("Which box?")
         pos = topos(choice)
         shape1(pos)
@@ -105,6 +110,7 @@ def game():
           break
         count += 1
 def win(playerset):
+  # A function to determine if a player has a winning set
   winSet = [
     ['a1','a2','a3'],
     ['b1','b2','b3'],
@@ -113,14 +119,13 @@ def win(playerset):
     ['a2','b2','c2'],
     ['a3','b3','c3'],
     ['a1','b2','c3'],
-    ['a3','b2','c1'],
+    ['a3','b2','c1']
   ]
   for i in winSet:
     if all(value in playerset for value in i):
       return True
-    
-  
 def main():
+  # The function that starts everything.
   display = turtle.Screen()
   displaysize=400
   display.screensize(canvwidth=displaysize, canvheight=displaysize)
