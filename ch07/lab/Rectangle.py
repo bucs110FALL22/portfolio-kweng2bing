6373175a9e2005cca0e2af5455182289d44a9104
kweng2 <kweng2@binghamton.edu>
2022-11-04 00:33:28 -0400

    ...

--- ch07/lab/Rectangle.py
+++ ch07/lab/Rectangle.py
@@ -1,8 +1,21 @@
 class Rectangle:
-    def __init__(self, x, y, h, w):
-        self.x = x
-        self.y = y
-        self.h = h
-        self.w = w
+    def __init__(self, x, y, height, width):
+        change= [x, y, height, width]
+        for i in range(len(change)):
+            j= change[i]
+            if j < 0:
+                change[i]=0
+        self.x = change[0]
+        self.y = change[1]
+        self.height = change[2]
+        self.width = change[3]
+        print(change)
 
     def __str__(self):
+        resultStr =f"(x:{self.x}, y:{self.y}, height:{self.height}, width:{self.width})"
+        print(resultStr)
+        # resultStr = f"x-vlue: {self.x}"
+        # resultStr += f"y-vlue: {self.y}"
+        # resultStr += f"h-vlue: {self.h}"
+        # resultStr += f"w-vlue: {self.w}"
+        return resultStr
--- ch07/lab/Surface.py
+++ ch07/lab/Surface.py
@@ -1,1 +1,10 @@
+import Rectangle
 class Surface:
+    def __init__(self, filename, x,y,h, w):
+        self.image = filename
+        self.rect = Rectangle.Rectangle( x, y, h, w)
+
+    def __getRect__(self):
+        # Returns the rectangle
+        return self.rect
+
