d35830c18fd0a177b7eaca4933a4f5447202aa79
Kalvin <111814372+kweng2bing@users.noreply.github.com>
2022-11-17 01:39:25 +0000

    test

--- ch08/lab/StringUtility.py
+++ ch08/lab/StringUtility.py
@@ -2,8 +2,10 @@ class StringUtility:
     # All the function are to do something to the string according to the lab.
     def __init__(self, string):
         self.string = string
+
     def __str__(self):
         return self.string
+
     def vowels(self):
         count = 0
         for element in self.string:
@@ -13,11 +15,13 @@ class StringUtility:
             return "many"
         else:
             return f"{count}"
+
     def bothEnds(self):
         if len(self.string) <= 2:
-            return  ""
+            return ""
         else:
             return f"{self.string[:2]}" + f"{self.string[-2:]}"
+
     def fixStart(self):
         strfix = ""
         if len(self.string) < 2:
@@ -26,11 +30,13 @@ class StringUtility:
             strfix = self.string[1:]
             strfix = strfix.replace(self.string[0], "*")
             return self.string[0] + strfix
+
     def asciiSum(self):
-        strascii =0
+        strascii = 0
         for element in self.string:
             strascii += ord(element)
         return strascii
+
     def cipher(self):
         indexchange = len(self.string)
         while len(self.string) > 26:
@@ -40,14 +46,14 @@ class StringUtility:
         for i in range(len(strcipher)):
             newlet = chr(ord(self.string[i]) + indexchange)
             ordvalue = ord(newlet)
-            if ordvalue in range(97,123):
+            if ordvalue == 32:
+                cryptography.append(" ")
+            elif ordvalue in range(97, 123):
                 cryptography.append(newlet)
-            elif ordvalue in range(65,91):
+            elif ordvalue in range(65, 91):
                 cryptography.append(newlet)
-            else: 
-                cryptography.append(chr(ord(newlet)-26))
- 
+            else:
+                cryptography.append(chr(ord(newlet) - 26))
+
         listtostring = ''.join(map(str, cryptography))
         return listtostring
-
-
